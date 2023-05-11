3e0ed77e51fc8b82fac10a8d27ae4447e7268523
crzstacoder <crazystarcoder@gmail.com>
2023-05-12 01:12:18 +0900

    move

--- thankyouwork/signup/models.py
+++ thankyouwork/signup/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
-
-class Posting(models.Model) :
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='postings')
-
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.title
-    
+        return self.username
--- thankyouwork/signup/views.py
+++ thankyouwork/signup/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.views import View
@@ -10,7 +7,7 @@ from .forms import signup
 class SignUpView(View):
     def get(self, request):
         form = signup()
-        return render(request, 'signup/signup.html', {'form': form})
+        return render(request, 'signup.html', {'form': form})
 
     def post(self, request):
         form = signup(request.POST)
@@ -22,4 +19,4 @@ class SignUpView(View):
             if user is not None:
                 login(request, user)
             return redirect('core:home')
-        return render(request, 'signup/signup.html', {'form': form})
+        return render(request, 'signup.html', {'form': form})
